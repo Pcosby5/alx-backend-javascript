2660a577700674788012440dffd02a3850e949cd
Pcosby5 <pcosby50@gmail.com>
2024-04-22 16:07:39 +0200

    added comments

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,15 @@
+/**
+ * Returns an array with two boolean values based on the input parameter.
+ * @param {boolean} trueOrFalse - Input parameter.
+ * @returns {Array} - An array containing two boolean values.
+ */
 export default function taskBlock(trueOrFalse) {
   let task = false;
   let task2 = true;
 
   if (trueOrFalse) {
-    task = true; // Removed 'let' to assign new value to existing variables
-    task2 = false; // Removed 'let' to assign new value to existing variables
+    task = true; // Removed 'let' to modify the outer scope variable
+    task2 = false; // Removed 'let' to modify the outer scope variable
   }
 
   return [task, task2];
