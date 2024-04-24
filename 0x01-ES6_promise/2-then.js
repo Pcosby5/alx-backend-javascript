ee715d0331982d83cd3a25e69435bdc74746ab31
Pcosby5 <pcosby50@gmail.com>
2024-04-24 13:35:06 +0200

    0x01-ES6_promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,14 +2,20 @@
  * Appends handlers to the provided promise.
  * @param {Promise} promise - The promise to handle.
  */
+// export default function handleResponseFromAPI(promise) {
+//   promise
+//     .then(() => {
+//       console.log('Got a response from the API');
+//       return { status: 200, body: 'success' };
+//     })
+//     .catch(() => {
+//       console.log('Got a response from the API');
+//       return new Error();
+//     });
+// }
 export default function handleResponseFromAPI(promise) {
-  promise
-    .then(() => {
-      console.log('Got a response from the API');
-      return { status: 200, body: 'success' };
-    })
-    .catch(() => {
-      console.log('Got a response from the API');
-      return new Error();
-    });
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
