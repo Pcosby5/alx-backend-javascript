ceda2987001668504b8e790294013fe82b01ac99
Pcosby5 <pcosby50@gmail.com>
2024-04-24 13:56:46 +0200

    0x01-ES6_promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -2,11 +2,14 @@
  * Prototype function to return a Promise.
  * @returns {Promise} A Promise.
  */
+// export default function getResponseFromAPI() {
+//   return new Promise((resolve) => {
+//     // Simulating API response after 1 second
+//     setTimeout(() => {
+//       resolve('API response data');
+//     }, 1000);
+//   });
+// }
 export default function getResponseFromAPI() {
-  return new Promise((resolve) => {
-    // Simulating API response after 1 second
-    setTimeout(() => {
-      resolve('API response data');
-    }, 1000);
-  });
+  return new Promise(() => {});
 }
