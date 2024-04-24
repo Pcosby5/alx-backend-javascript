14c813baf253c91a5980fb25664b049a750a5204
Pcosby5 <pcosby50@gmail.com>
2024-04-24 13:52:58 +0200

    0x01-ES6_promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -6,13 +6,22 @@ import { uploadPhoto, createUser } from './utils';
  * Logs firstName and lastName to the console on successful resolution.
  * Logs 'Signup system offline' in case of an error.
  */
+// export default function handleProfileSignup() {
+//   Promise.all([uploadPhoto(), createUser()])
+//     .then((values) => {
+//       const [photo, user] = values;
+//       console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+//     })
+//     .catch(() => {
+//       console.log('Signup system offline');
+//     });
+// }
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then((values) => {
-      const [photo, user] = values;
-      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+    .catch(() => console.log('Signup system offline'));
 }
+
