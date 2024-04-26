d0ad11b7c52b2db0d80f826b24b988cae05815a6
Pcosby5 <pcosby50@gmail.com>
2024-04-26 17:49:35 +0200

    100-evcar.js

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,43 @@
+import Car from './10-car';
+
+/**
+ * Class representing an Electric Vehicle Car.
+ */
+export default class EVCar extends Car {
+  /**
+   * Create an EVCar.
+   * @param {string} brand - The brand of the car.
+   * @param {string} motor - The type of motor of the car.
+   * @param {string} color - The color of the car.
+   * @param {string} range - The range of the car.
+   */
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this.range = range;
+  }
+
+  /**
+   * Get the range of the car.
+   * @returns {string} The range of the car.
+   */
+  get range() {
+    return this._range;
+  }
+
+  /**
+   * Set the range of the car.
+   * @param {string} value - The range to set.
+   */
+  set range(value) {
+    this._range = value;
+  }
+
+  /**
+* Clone the car. Returns an instance of Car.
+* @returns {Car} A new instance of the Car class.
+*/
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+    return new Species();
+  }
+}
