d607540ef36c46df3b8fca90f4094de1614e432c
Pcosby5 <pcosby50@gmail.com>
2024-04-26 18:00:24 +0200

    100-evcar.js

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -39,8 +39,7 @@ export class StudentHolberton {
 
   // replace self with this
   get fullStudentDescription() {
-    return `${this._firstName} ${this._lastName} - \
-     ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
 
