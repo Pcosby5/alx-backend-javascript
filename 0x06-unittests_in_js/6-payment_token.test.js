e10f398c14f701aeb32cf97339f79bcf04f0449a
Pcosby5 <pcosby50@gmail.com>
2024-06-20 13:43:23 +0200

    async tests with done

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,13 @@
+const { expect } = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', function () {
+    it('should return a successful response when success is true', function (done) {
+        getPaymentTokenFromAPI(true).then((response) => {
+            expect(response).to.include({ data: 'Successful response from the API' });
+            done();
+        }).catch((error) => {
+            done(error);
+        });
+    });
+});
